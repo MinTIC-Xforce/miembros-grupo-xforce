58de067097ba3332cf925df1fa44935a7a454760
danhcertuche <danielcert3@gmail.com>
2022-08-25 03:48:19 -0500

    InitialCommit Clase MovimientoDinero

---
+++ MovimientoDinero.java
@@ -0,0 +1,36 @@
+
+package com.mycompany.ejemplo.mavenproject1;
+
+
+public class MovimientoDinero {
+    
+    //Atributos
+    private float montoMovimiento;
+    private String conceptoMovimiento;
+
+    //Constructor
+    public MovimientoDinero(float montoMovimiento, String conceptoMovimiento) {    
+        this.montoMovimiento = montoMovimiento;
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    //Getters y Setters
+    public float getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(float montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+    
+}
+
+
